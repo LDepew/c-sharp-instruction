1ea08adbda5050e670c7f919cdf8db6bfc9c9c71
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-16 10:19:03 -0400

    created bmdb Movie Class

---
+++ bmdb-console/bmdb-console/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bmdb_console {
+    class Movie {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Rating { get; set; }
+        public int Year { get; set; }
+        public String Director { get; set; }
+
+
+        public Movie() {
+
+        }
+
+        public Movie(int Id, String Title, String Rating, int Year, String Director) {
+            this.Id = Id;
+            this.Title = Title;
+            this.Rating = Rating;
+            this.Year = Year;
+            this.Director = Director;
+
+        }
+
+
+
+
+
+        public override string ToString() {
+            return $"Movie: {Id}, {Title}, {Rating}, {Year}, {Director}";
+        }
+    }
+}
---
+++ bmdb-console/bmdb-console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace bmdb_console {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the bmdb Console");
+
+            Console.Write("Enter movie ID: ");
+            int id = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter movie title: ");
+            String title = Console.ReadLine();
+            Console.Write("Enter movie rating: ");
+            String rating = Console.ReadLine();
+            Console.Write("Enter movie year: ");
+            int year = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter movie director: ");
+            String director = Console.ReadLine();
+            Movie m1 = new Movie(id, title, rating, year, director);
+
+
+            Console.WriteLine(m1);
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
