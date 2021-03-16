efc96b469480fa4fd12da79691e19a3c49f057c8
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-16 14:07:08 -0400

    add lists and maps

--- lists-and-maps/lists-and-maps/Program.cs
+++ lists-and-maps/lists-and-maps/Program.cs
@@ -1,9 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lists_and_maps {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lists and Maps!");
+            List<String> singers = new List<String>();
+            singers.Add("Bob Marley");
+            singers.Add("Dave Grohl");
+            singers.Add("Freddie Mercury");
+            singers.Add("Bob Dylan");
+
+            singers.Sort();
+
+            foreach (String item in singers) {
+            Console.WriteLine(item);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Bye");
         }
     }
 }
